d49178bb012a54ddffb5831569a3625b563f4072
mahiawasthi23 <mahima23@navgurukul.org>
2025-11-07 14:46:43 +0530

    work in  tracking Routes and trackingController

--- controllers/trackingController.js
+++ controllers/trackingController.js
@@ -3,39 +3,49 @@ import User from "../models/User.js";
 import moment from "moment";
 
 export const addTracking = async (req, res) => {
-  try {
-    const studentId = req.user.id;
-    const { date, status, document } = req.body;
-
-    if (!date || !status) {
-      return res.status(400).json({ message: "Date and Status are required." });
-    }
-
-    const user = await User.findById(studentId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found." });
-    }
+  try {
+    const studentId = req.user.id;
     
+   
+    const { fullName, email, date, studentType } = req.body; 
+    const uploadedFile = req.file; 
 
-    const formattedDate = moment(date).format("YYYY-MM-DD");
+    if (!date || !studentType || !fullName || !email) {
+      return res.status(400).json({ message: "All required fields are missing." });
+    }
 
-    const tracking = new Tracking({
-      studentId,
-      name: user.name,
-      email: user.email,
-      date: formattedDate,
-      status,
-      document,
-    });
+    const user = await User.findById(studentId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    
+    
+    let documentUrl = '';
+    if (uploadedFile) {
+      
+      
+        documentUrl = uploadedFile.originalname;
+    }
 
-    await tracking.save();
+    const formattedDate = moment(date).format("YYYY-MM-DD");
 
-    res.status(201).json({
-      message: "Student lifecycle entry added successfully!",
-      tracking,
-    });
-  } catch (error) {
-    console.error("Error adding tracking entry:", error);
-    res.status(500).json({ message: "Server error" });
-  }
+    const tracking = new Tracking({
+      studentId,
+      name: fullName, 
+      email: email,
+      date: formattedDate,
+      status: studentType, 
+      document: documentUrl, 
+    });
+
+    await tracking.save();
+
+    res.status(201).json({
+      message: "Student lifecycle entry added successfully!",
+      tracking,
+    });
+  } catch (error) {
+    console.error("Error adding tracking entry:", error);
+    res.status(500).json({ message: "Server error during file/tracking submission." });
+  }
 };
