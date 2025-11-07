f7e17dac8e8ee9bb0b445f68531a73a89b3380e6
nasrinakhatun23 <nasrinakhatun23@navgurukul.org>
2025-11-07 22:41:18 +0530

    white space remove

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -4,7 +4,6 @@ import { protect, adminOnly } from "../middleware/authMiddleware.js";
 import { applyLeave, approveLeave, rejectLeave, getAllLeaves } from "../controllers/leaveController.js";
 
 const router = express.Router();
-
 router.post("/apply", protect, applyLeave);
 router.get("/", protect, adminOnly, getAllLeaves);
 router.put("/approve/:id", protect, adminOnly, approveLeave);
